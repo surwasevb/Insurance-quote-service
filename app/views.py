dad37e74f24ec66e30d5beb956dac5041cd58099
surwasevb <surwasevbit@gmail.com>
2026-08-24 16:38:42 +0530

    add api to accept create customer request

--- app/views.py
+++ app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+import logging
+
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from app.models import Customer
+from app.serializers import CustomerSerializer
+
+logger = logging.getLogger(__name__)
+
 
 # Create your views here.
+class CustomerView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
+    def post(self, request, *args, **kwargs):
+        logger.info(F"Received Create request for customer {request.data}")
+
+        customer_serializer = CustomerSerializer(data=request.data)
+        customer_serializer.is_valid(raise_exception=True)
+        customer: Customer = customer_serializer.save()
+
+        logger.info(F"Successfully created user with {customer.id}")
+        return Response(status=200, content_type="application/json", data={"id": customer.id})
--- insurance_quote_service/urls.py
+++ insurance_quote_service/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app.views import CustomerView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/v1/create_customer/", CustomerView.as_view(), name="create_customer"),
 ]
