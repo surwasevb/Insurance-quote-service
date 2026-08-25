b6852147755e96da9190b4621e135cecf6374c0e
surwasevb <surwasevbit@gmail.com>
2026-08-25 18:46:57 +0530

    add new serializers

--- app/serializers.py
+++ app/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from app.models import Customer, PolicyState
+from app.models import Customer, Policy, PolicyState, PolicyStateHistory
 
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -18,3 +18,17 @@ class QuoteCreateSerializer(serializers.Serializer):
 class QuoteUpdateSerializer(serializers.Serializer):
     policy_id = serializers.UUIDField(required=True)
     status = serializers.ChoiceField(required=True, choices = PolicyState.choices)
+
+class PolicySerializer(serializers.ModelSerializer):
+    customer = CustomerSerializer(read_only=True)
+
+    class Meta:
+        model = Policy
+        fields = ["id", "customer", "type", "premium", "cover", "state", "created_at", "updated_at"]
+        read_only_fields = fields
+
+
+class PolicyStateHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PolicyStateHistory
+        fields = ["from_state", "to_state", "changed_at"]
