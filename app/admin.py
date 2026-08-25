c811656e88557e3106a0854fe014bb10812cb405
surwasevb <surwasevbit@gmail.com>
2026-08-25 20:06:53 +0530

    add policy and policy state history in admin portal

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from app.models import Customer
+from app.models import Customer, Policy, PolicyStateHistory
 
 
 # Register your models here.
@@ -8,3 +8,15 @@ from app.models import Customer
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ["id", "first_name", "last_name", "dob", "created_at", "updated_at"]
     search_fields = ["first_name", "last_name", "dob"]
+
+
+@admin.register(Policy)
+class PolicyAdmin(admin.ModelAdmin):
+    list_display = ["id", "type", "premium", "cover", "created_at", "updated_at"]
+    search_fields = ["type", "id"]
+
+
+@admin.register(PolicyStateHistory)
+class PolicyStateHistoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "from_state", "to_state", "changed_at"]
+    search_fields = ["from_state", "to_state", "changed_at"]
